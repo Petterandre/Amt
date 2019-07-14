7f8ea7122abcfcbf2cc0b9728d137261d8ada1f7
Petterandre <petterbraten@gmail.com>
2019-07-14 21:46:11 +0200

    2nd

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
-window.onscroll = function() {scrollFix()};
+$('a[href^="#"]').on('click', function(event) {
 
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
+    var target = $(this.getAttribute('href'));
 
-function scrollFix() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+
+
+document.getElementById("wrapper").addEventListener("click", displayDate);
+
+function displayDate() {
+	console.log("Hi");
+}
+
+function menuHide() {
+  var x = document.getElementById("navigation");
+  switch (x.classList.contains("responsive")) {
+  	case true:
+  	console.log("Yes");
+  	x.classList.remove("responsive");
+  	break;
+  	case false:
+  	x.classList.add("responsive");
+  	console.log("No");
+  	break;
+  	console.log("Neither");
   }
 }
