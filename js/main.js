b2dd1f92ea495bfcf1dcbe7361d185a6b54a3520
Petterandre <petterbraten@gmail.com>
2019-07-15 13:50:22 +0200

    FinishedDesign1

--- js/main.js
+++ js/main.js
@@ -1,34 +1,46 @@
+document.getElementById("wrapper").addEventListener("click", unclickMenu);
+
 $('a[href^="#"]').on('click', function(event) {
 
     var target = $(this.getAttribute('href'));
+	var at = $(this.getAttribute('id'));
+    var sel = document.getElementById(at.selector);
+    removeActive();
+    sel.classList.add("active");	
 
     if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
+    	event.preventDefault();
+		$('html, body').stop().animate({
+        	scrollTop: target.offset().top - 45
+        }, 50);
     }
 
 });
 
-
-document.getElementById("wrapper").addEventListener("click", displayDate);
-
-function displayDate() {
-	console.log("Hi");
+function removeActive() {
+	$('.active').removeClass('active');
 }
 
+function unclickMenu() {
+	var x = document.getElementById("navigation");
+
+ 	switch (x.classList.contains("responsive")) {
+	  	case true:
+	  	x.classList.remove("responsive");
+	  	break;
+  	}
+}
+
+
 function menuHide() {
-  var x = document.getElementById("navigation");
-  switch (x.classList.contains("responsive")) {
-  	case true:
-  	console.log("Yes");
-  	x.classList.remove("responsive");
-  	break;
-  	case false:
-  	x.classList.add("responsive");
-  	console.log("No");
-  	break;
-  	console.log("Neither");
+	var x = document.getElementById("navigation");
+	switch (x.classList.contains("responsive")) {
+		case true:
+		x.classList.remove("responsive");
+		break;
+		case false:
+		x.classList.add("responsive");
+		break;
+		
   }
 }
